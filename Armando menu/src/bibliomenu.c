b0f605cd16dde5fdcc5da7634112b6a91fb6a05f
victoriarodr <vickyrodr69@gmail.com>
2022-09-08 10:43:02 -0300

    bibliotecas

---
+++ Armando menu/src/bibliomenu.c
@@ -0,0 +1,22 @@
+/*
+ * bibliomenu.c
+ *
+ *  Created on: 8 sep. 2022
+ *      Author: vicky
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int menu ()
+{
+	int retorno;
+
+	printf("\n1) Iniciar \n"
+			"2) Procesar \n"
+			"3) Finalizar \n"
+			"4) Salir \n");
+	printf("\n Ingrese una opción: ");
+	scanf("%d", &retorno);
+
+	return retorno;
+}
+
